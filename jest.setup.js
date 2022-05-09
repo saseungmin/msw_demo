559f48e3974728036828fa54f263f0323521a0b4
saseungmin <dbd02169@naver.com>
2022-05-09 23:57:45 +0900

    [Feature] MSW 초기 세팅하라
    - msw 초기 세팅

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,10 @@
+import server from './src/mocks/server';
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-plugin-context/setup';
 import 'given2/setup';
+
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
